c1c4aefd8e20961742d97e2e6c18c849a982fb38
luigi massa <lmassa@bwlab.it>
2023-03-16 22:51:47 +0100

    todo

--- Classes/MarketplaceOrderSplit.php
+++ Classes/MarketplaceOrderSplit.php
@@ -82,8 +82,7 @@ class MarketplaceOrderSplit
             );
             $id_order_state = $this->mainOrder->getCurrentState();
             $sellerOrder->setCurrentState($id_order_state);
-            //TODO add payment per order beacause PS show message error Payment minus X
-
+            //TODO impostare anche l'order history per li split altrimenti il cliente non vede lo stato nel frontend
             return $this->createSellerOrder($sellerOrder->id);
 
         } catch (\Exception $exception) {
